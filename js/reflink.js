e79dfe931fe86ab974e5e856603f0abd46011f7c
shulhaha <shulhaha@protonmail.com>
2023-07-06 00:52:44 +0300

    init

---
+++ js/reflink.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("a").click(function(){
+    $("#reflink*").attr("href", "https://vpartnervavada.com/?promo=09eb7607-2927-4d3a-aa93-0f417cbc2d62&target=register");
+  });
+});
